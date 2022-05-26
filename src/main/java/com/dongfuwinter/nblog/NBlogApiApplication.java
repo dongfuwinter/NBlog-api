96347b1582a389d35704106a494d3124d8bd28a5
dongfuwinter <d18638188035@163.com>
2022-05-26 15:50:08 +0800

    init project

---
+++ src/main/java/com/dongfuwinter/nblog/NBlogApiApplication.java
@@ -0,0 +1,13 @@
+package com.dongfuwinter.nblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NBlogApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NBlogApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dongfuwinter/nblog/NBlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongfuwinter.nblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NBlogApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
